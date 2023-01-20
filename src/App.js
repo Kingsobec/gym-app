4845071b0940d1ab0bef353bd06347f564909db5
kingsobec <kingsobec@gmail.com>
2023-01-20 23:33:59 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React from 'react'
-import { Route, Routes } from "react-router-dom"
-import { Box } from '@mui/material'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { Box } from "@mui/material";
+import ExerciseDetail from "./pages/ExerciseDetail";
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer"
+
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <Box width="400px">
+      <Navbar />
+      <Route path="/" element={<Home />} />
+      <Route path="/exercise/:id" element={<ExerciseDetail />} />
+      <Footer />
+    </Box>
+  );
+};
 
-export default App
+export default App;
